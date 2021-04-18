778cea58c7abe1b6dbfaf45babbbfbbe0c78c73f
gleeful-node-f5d4 <abdulazeezalabbasi@yahoo.com>
2021-04-18 06:34:17 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react'
 export default class Keypad extends Component {
   
   handleChange = () => {
-    
+    console.log('Entering password')
   }
   render() {
     return (
       <div>
-        <input type="password" onChange={this.handleChange}/>
+        <input type="password" onKeyUp={this.handleChange}/>
       </div>
     )
   }
