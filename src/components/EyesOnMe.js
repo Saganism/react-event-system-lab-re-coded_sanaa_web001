df7089dd70e2ebe13d24cefb5eaf8ea4074d8a5f
gleeful-node-f5d4 <abdulazeezalabbasi@yahoo.com>
2021-04-18 06:42:34 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 
 export default class EyesOnMe extends Component {
   
+  handleFocus = ()=>{
+    
   
   render() {
     return (
       <div>
-        <button onFocus={this.handleFocus}>Eyes on me</button>
+        <button onFocus={this.handleFocus} onBlur={this.handleBlur}>Eyes on me</button>
       </div>
     )
   }
